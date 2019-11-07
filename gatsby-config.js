b3b7178e248a250c908d9bd7d2e4228b6ed558c4
olethrosjv <chefjoevaughn@gmail.com>
2019-11-07 15:10:16 -0800

    meta changes to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Components`,
+    description: `Made with the Gatsby default starter. This is a library of reusable styled components for Gatsby websites.`,
+    author: `Joe Vaughn`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
