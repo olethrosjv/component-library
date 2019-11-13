82fded5633c2814b9b99358de9b59094aedbad6c
olethrosjv <chefjoevaughn@gmail.com>
2019-11-13 14:22:49 -0800

    added a text-only header component

--- src/pages/sidebar-page.js
+++ src/pages/sidebar-page.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
-import "components/layout.css"
+import "../components/layout.css"
 
 
 const SideBarPage = () => (
---
+++ src/pages/text-header-page.js
@@ -0,0 +1,12 @@
+import React from "react"
+import TextOnlyHeader from "../components/textOnlyHeader"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <TextOnlyHeader headerText="This is a Text-Only Header." />
+    <p>Such wow. Very React.</p>
+  </div>
+)
+
+
+
