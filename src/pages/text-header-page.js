5128cc95686a1646587fe4496f113b47fa27c898
olethrosjv <chefjoevaughn@gmail.com>
2019-11-13 14:26:00 -0800

    minor very minor edits

--- src/pages/text-header-page.js
+++ src/pages/text-header-page.js
@@ -4,7 +4,7 @@ import TextOnlyHeader from "../components/textOnlyHeader"
 export default () => (
   <div style={{ color: `teal` }}>
     <TextOnlyHeader headerText="This is a Text-Only Header." />
-    <p>Such wow. Very React.</p>
+    <p>This is a text only header component with no background, borders, or styling aside from text color.</p>
   </div>
 )
 
